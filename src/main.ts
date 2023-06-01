cdb06e1984d850da2f4ba1caf0f40d53889ce00a
Adam Walker <adamwalker101@gmail.com>
2023-06-01 11:21:47 +0100

    Hide swagger (Speeds up build)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+//import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'typeorm';
 
 dotenv.config();
@@ -13,18 +13,18 @@ async function bootstrap() {
 
   app.enableCors();
 
-  const config = new DocumentBuilder()
-    .setTitle('Nitr0gen App Server')
-    .setVersion('1.0')
-    //.addTag('security')
-    .addApiKey({
-      type: 'apiKey',
-      name: 'X-API-KEY',
-      in: 'header',
-    }, 'X-API-KEY')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('Nitr0gen App Server')
+  //   .setVersion('1.0')
+  //   //.addTag('security')
+  //   .addApiKey({
+  //     type: 'apiKey',
+  //     name: 'X-API-KEY',
+  //     in: 'header',
+  //   }, 'X-API-KEY')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
   const PORT = Number(process.env.PORT) || 8080;
   await app.listen(PORT);
